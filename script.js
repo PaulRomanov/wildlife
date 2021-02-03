bbf37327bb487c222689446296fdf302d3af6d3c
PaulRomanov <ramosha@mail.ru>
2021-02-03 20:43:34 +0300

    feat: add slider

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 //очищает поле ввода Email
+
 document.getElementById("clearButton").onclick = function(e) {
   document.getElementById("textInput").value = "";
 }
 
+// СЛАЙДЕР START
+
+
+// нахожу кнопки
+let prevBtn = document.getElementById('prev');
+let nextBtn = document.getElementById('next');
+
+// привязываю обработчики событий
+prevBtn.addEventListener('click', onPrevBtnClick);
+nextBtn.addEventListener('click', onNextBtnClick);
+
+
+// проверка нажатия на кнопки
+function onPrevBtnClick (){
+  console.log('prev clicked');
+};
+
+function onNextBtnClick (){
+  console.log('next clicked');
+};
